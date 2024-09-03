1ed972d814b03721daba2eaa51b5ca0efda3469d
jai <nireshpandian19@gmail.com>
2024-09-03 20:53:06 +0530

    Added scheduler

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const { exec } = require('child_process');
 
+
+const runScript = () => {
+
+    console.log("Running the script")
 exec('bash devResultChecker.sh', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`Stderr: ${stderr}`);
-    return;
-  }
-  console.log(`Stdout: ${stdout}`);
-});
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Stderr: ${stderr}`);
+      return;
+    }
+    console.log(`Stdout: ${stdout}`);
+  });
+}
+
+runScript();
+setInterval(runScript, 300000 )
